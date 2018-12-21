28886cb53f87abd0a64e4510ccca52060e870173
farfaniriss <iris.farfan.c@gmail.com>
2018-12-21 12:53:29 -0500

    using ng2-charts library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarComponent } from './components/bar/bar.component';
+import { DoughnutComponent } from './components/doughnut/doughnut.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarComponent,
+    DoughnutComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
